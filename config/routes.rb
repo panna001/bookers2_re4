810c54afeca8d8e310b0870f6b0284fa62a5c260
panna001 <sonar.pocket.0926@gmail.com>
2021-02-09 09:10:07 +0000

    [Add]ログイン・サインイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'home#top'
-  get 'home/about'
   devise_for :users
+  root "users#index"
+  get 'home/about'
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:index, :show, :create, :edit, :destoy, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 2021_02_09_061516) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
+    t.text "introduction"
+    t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
