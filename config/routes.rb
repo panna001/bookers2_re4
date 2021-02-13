4a449d8299ac1a3442c54760b45d57d8a2f3b3d2
panna001 <sonar.pocket.0926@gmail.com>
2021-02-13 01:57:25 +0000

    [Add]リレーションシップ側のリレーションとroute

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'relationships/create'
-  get 'relationships/destroy'
   devise_for :users
   root "home#top"
   get 'home/about'
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resources :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
+  end
   resources :books, only: [:index, :show, :create, :edit, :destroy, :update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
