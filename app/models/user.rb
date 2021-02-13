2fa213e7e2e3c754c1af52034a51244f7ebcd8dd
panna001 <sonar.pocket.0926@gmail.com>
2021-02-13 02:03:28 +0000

    [Add]リレーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
  has_many :books, dependent: :destroy
  has_many :favorites, dependent: :destroy
  has_many :book_comments, dependent: :destroy
+ has_many :relationships, class_name: "Relationship", foreign_key: :following_id
+ has_many :followings, through: :relationships, source: :follower
+ has_many :reverse_relationships, class_name: "Relationship", foreign_key: :following_id
+ has_many :followers, through: :reverse_relationships, source: :following
  validates :name, uniqueness: true, length: {in: 2..20}
  validates :introduction, length: {maximum: 50}
 end
